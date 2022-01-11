8f2a14f782285e32311ee51994b2ea6a8bb86c72
lkook <lucasheinrich.kook@uzh.ch>
2022-01-11 15:44:20 +0100

    revised version of figure 4

--- figures/vis-app-bh2.R
+++ figures/vis-app-bh2.R
@@ -10,8 +10,9 @@ library(patchwork)
 theme_set(theme_pubr())
 data("BostonHousing2", package = "mlbench")
 
-bpath <- "../experiments/"
-mlabs <- c("Lm", "c*'-'*probit", "c*'-'*logiti", "c*'-'*logit")
+bpath <- "results/app-bh2-revision/"
+mlabs <- c("Lm", "c*'-'*probit", "c*'-'*logiti", "c*'-'*logit~exact", 
+           "c*'-'*logit~censored")
 
 cfs <- data.frame(path = list.files(bpath, pattern = "cfx")) %>% 
   separate(path, into = c("app", "bh", "mod", "xi", "cfx"), sep = "-", 
@@ -21,27 +22,26 @@ cfs <- data.frame(path = list.files(bpath, pattern = "cfx")) %>%
   mutate(dat = lapply(paste0(bpath, path), read.csv)) %>% 
   unnest(dat) %>% 
   gather("predictor", "estimate", crim:lstat) %>% 
-  mutate(mod = factor(mod, levels = c("Lm", "BoxCox", "Colr", "Colrr"), 
+  mutate(mod = factor(mod, levels = c("Lm", "BoxCox", "Colr", "Colre", "Colrr"), 
                       labels = mlabs),
          xi = factor(xi, levels = c(0, 10^(0:4)), labels = c("-infinity", 0:4)),
-         estimate = ifelse(mod %in% c("c*'-'*logit", "c*'-'*logitr"), 
-                           -estimate, estimate))
+         estimate = ifelse(str_detect(mod, "logit"), -estimate, estimate))
 
 p1 <- cfs %>% 
-  filter(model == "anchor", predictor %in% c("indus", "lstat", "nox", "rm"), 
+  filter(model == "anchor", predictor %in% c("indus", "lstat", "nox", "rm"),
          mod != mlabs[3]) %>% 
   ggplot(aes(x = predictor, y = estimate, color = xi)) +
   geom_hline(aes(yintercept = 0), col = "gray65", lty = 2) +
-  geom_boxplot() +
+  geom_boxplot(outlier.size = rel(0.5)) +
   facet_grid(~ mod, labeller = label_parsed) + 
-  scale_color_viridis_d(name = parse(text = "log[10]~xi"), 
+  scale_color_viridis_d(name = parse(text = "log[10]~xi"),
                         labels = parse(text = levels(cfs$xi))) +
   theme(text = element_text(size = 12)) +
   labs(y = expression(hat(beta)[CV]), x = element_blank()) + 
   guides(color = guide_legend(nrow = 1))
 
 lls <- data.frame(path = list.files(bpath, pattern = "logLik")) %>% 
-  separate(path, into = c("app", "bh", "mod", "xi", "cfx"), sep = "-", 
+  separate(path, into = c("app", "bh", "mod", "xi", "cfx"), sep = "-",
            remove = FALSE) %>% 
   separate(xi, into = c("parm", "xi"), sep = "xi") %>% 
   select(-app, -bh, -parm, -cfx) %>% 
@@ -52,10 +52,9 @@ lls <- data.frame(path = list.files(bpath, pattern = "logLik")) %>%
   })) %>% 
   unnest(dat) %>% 
   gather("model", "logLik", plain:anchor) %>%
-  mutate(mod = factor(mod, levels = c("Lm", "BoxCox", "Colr", "Colrr"), 
+  mutate(mod = factor(mod, levels = c("Lm", "BoxCox", "Colr", "Colre", "Colrr"), 
                       labels = mlabs),
-         xi = factor(xi, levels = c(0, 10^(0:4)), 
-                     labels = paste0(c("-infinity", 0:4))))
+         xi = factor(xi, levels = c(0, 10^(0:4)), labels = paste0(c("-infinity", 0:4))))
 
 outliers <- lls %>% 
   filter(model == "anchor", mod != mlabs[3]) %>% 
@@ -65,14 +64,14 @@ outliers <- lls %>%
 p2 <- lls %>% 
   filter(model == "anchor", mod != mlabs[3]) %>%
   ggplot(aes(x = xi, y = -logLik, color = xi)) +
-  geom_boxplot(show.legend = TRUE) +
+  geom_boxplot(outlier.size = rel(0.5), show.legend = TRUE) +
   facet_grid(~ mod, labeller = label_parsed) +
   labs(x = expression(log[10]~xi), y = expression(NLL[CV])) +
   theme(text = element_text(size = 12)) +
   scale_x_discrete(labels = parse(text = levels(lls$xi))) +
-  scale_color_viridis_d(name = parse(text = "log[10]~xi"), 
+  scale_color_viridis_d(name = parse(text = "log[10]~xi"),
                         labels = parse(text = levels(cfs$xi))) +
-  geom_path(aes(group = env), data = outliers, alpha = 0.5) +
+  geom_path(aes(group = env), data = outliers, lwd = 0.5) +
   ggrepel::geom_text_repel(aes(label = env),
                            data = outliers %>% filter(xi == "-infinity"),
                            nudge_y = 1.5, nudge_x = 2, show.legend = FALSE,
@@ -80,6 +79,7 @@ p2 <- lls %>%
   guides(color = guide_legend(nrow = 1))
         
 
-ggarrange(p2 + labs(tag = "A"), p1 + labs(tag = "B"), ncol = 1, align = "v",
+ggarrange(p2 + labs(tag = "a"), p1 + labs(tag = "b"), ncol = 1, align = "v",
           common.legend = TRUE, legend = "top")
-ggsave("figures/vis-app-bh2-xi.pdf", height = 5, width = 7)
+
+ggsave("figures/vis-app-bh2-xi.eps", height = 5, width = 9)
