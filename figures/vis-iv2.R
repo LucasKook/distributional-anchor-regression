782d6b48eb142e6b259c29de1e79f562a86d79cf
lkook <lucasheinrich.kook@uzh.ch>
2022-01-12 09:39:17 +0100

    figure 9

--- figures/vis-iv2.R
+++ figures/vis-iv2.R
@@ -9,9 +9,10 @@ library(ggpubr)
 library(patchwork)
 theme_set(theme_pubr())
 
-# Read coefs --------------------------------------------------------------
+Kwhich <- "K==10"
+bpath <- "results/iv2/" 
 
-bpath <- "results"
+# Read coefs --------------------------------------------------------------
 
 cfx_files <- 
   data.frame(files = list.files(bpath, pattern = "cfx"), stringsAsFactors = FALSE)
@@ -23,15 +24,14 @@ lbncl <- paste0("K==", ncls)
 lvs <- paste0("shift", ss <- c(0, 1, 1.8, 3))
 lbs <- paste0("do(A==", ss, ")")
 lvba <- paste0("ba", bas <- c(-1, 0.5, 1, 2))
-lbba <- paste0("zeta[2]==", bas)
+lbba <- paste0("M[X]==", bas)
 lvxi <- paste0("xi", xis <- c(0, 10^(0:4)))
 lbxi <- c("-infinity", 0:4)
 
 dat <- cfx_files %>% 
   mutate(files_sans_ext = tools::file_path_sans_ext(files)) %>% 
   separate(files_sans_ext, into = c("type", "model", "sim", "parms"), sep = "_") %>% 
-  separate(parms, into = c("n", "ncl", "bx", "ba", "bh", "sd", "shift", "xi"), 
-           sep = "-(?![0-9])") %>%
+  separate(parms, into = c("n", "ncl", "bx", "ba", "bh", "sd", "shift", "xi"), sep = "-(?![0-9])") %>%
   mutate(n = factor(n, levels = lvn, labels = lbn),
          ncl = factor(ncl, levels = lvncl, labels = lbncl),
          shift = factor(shift, levels = lvs, labels = lbs),
@@ -41,7 +41,7 @@ dat <- cfx_files %>%
   unnest(cols = dat)
 
 p1 <- dat %>% 
-  filter(ncl == "K==10", n == "n==1000", ba != "zeta[2]==-1", model == "anchor") %>% 
+  filter(ncl == Kwhich, n == "n==1000", ba != "M[X]==-1", model == "anchor") %>% 
   mutate(shift = "do(A%in%'{'*0*','*1*','*1.8*','*3*'}')") %>% 
   ggplot(aes(x = xi, y = x, color = xi)) +
   geom_boxplot(show.legend = FALSE) +
@@ -60,8 +60,7 @@ ll_files <-
 dat_ll <- ll_files %>% 
   mutate(files_sans_ext = tools::file_path_sans_ext(files)) %>% 
   separate(files_sans_ext, into = c("type", "model", "sim", "parms"), sep = "_") %>% 
-  separate(parms, into = c("n", "ncl", "bx", "ba", "bh", "sd", "shift", "xi"), 
-           sep = "-(?![0-9])") %>%
+  separate(parms, into = c("n", "ncl", "bx", "ba", "bh", "sd", "shift", "xi"), sep = "-(?![0-9])") %>%
   mutate(n = factor(n, levels = lvn, labels = lbn),
          ncl = factor(ncl, levels = lvncl, labels = lbncl),
          shift = factor(shift, levels = lvs, labels = lbs),
@@ -71,23 +70,28 @@ dat_ll <- ll_files %>%
   unnest(cols = dat)
 
 probs <- seq(0, 1, length.out = 1e3)
-sum_dat <- dat_ll %>% 
+sum_dat <- dat_ll %>%
+  gather("run", "logLik", starts_with("V")) %>%
+  group_by(model, n, ncl, bx, ba, bh, sd, shift, xi) %>%
+  summarise(qn = quantile(logLik, p = probs)) %>%
+  mutate(p = probs)
+
+sum_dat2 <- dat_ll %>% 
   gather("run", "logLik", starts_with("V")) %>% 
   group_by(model, n, ncl, bx, ba, bh, sd, shift, xi, run) %>% 
   summarise(NLL = mean(logLik))
 
-p3 <- sum_dat %>% filter(shift != "do(A==0)", ba != "zeta[2]==-1", 
-                         model == "anchor") %>% 
+p3 <- sum_dat2 %>% filter(shift != "do(A==0)", ba != "M[X]==-1", model == "anchor") %>% 
   ggplot(aes(x = xi, y = NLL, color = xi)) +
   geom_boxplot(show.legend = FALSE, outlier.size = 1) +
   facet_grid(shift ~ ba, labeller = label_parsed) +
-  scale_color_viridis_d(name = expression(log[10]~xi), 
-                        labels = parse(text = levels(sum_dat$xi))) +
+  scale_color_viridis_d(name = expression(log[10]~xi), labels = parse(text = levels(sum_dat2$xi))) +
   labs(x = expression(log[10]~xi), y = "NLL") +
   theme(text = element_text(size = 9)) +
   scale_x_discrete(labels = parse(text = c("-infinity", 0:4))) +
   guides(color = guide_legend(direction = "horizontal", nrow = 1))
 
-ggarrange(p3 + labs(tag = "A"), p1 + labs(tag = "B"), nrow = 2, 
+ggarrange(p3 + labs(tag = "a"), p1 + labs(tag = "b"), nrow = 2, 
           heights = c(2, 1), common.legend = FALSE, align = "v")
-ggsave("figures/vis-iv2.pdf", height = 5.5, width = 6.5)
+
+ggsave("figures/vis-iv2.eps", height = 5.5, width = 6.5)
