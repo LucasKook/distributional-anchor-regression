af111eb7e6b53193ed345687e7067d749ae3a0db
lkook <lucasheinrich.kook@uzh.ch>
2022-01-12 08:59:50 +0100

    revised figure 6

--- figures/vis-la.R
+++ figures/vis-la.R
@@ -9,40 +9,55 @@ library(ggpubr)
 library(patchwork)
 theme_set(theme_pubr())
 
-bpath <- "../experiments/"
-
 # Read results ------------------------------------------------------------
 
-res_lm <- read.csv(file.path(bpath, "scenario-la-lin.csv")) %>% 
-  mutate(quantile = (0:10)/10) %>% 
-  gather(key = "model", value = "mean_ape", mean_ape_anchor:mean_ape_plain)
-res_Lm <- read.csv("results/la/scenario-la-Lm.csv") %>%
+res_lm <- read.csv("scenario-la-lin.csv") %>% 
+  gather(key = "model", value = "ape", ape_anchor:ape_plain) %>% 
+  separate(model, into = c("jnk", "model"), sep = "_")
+res_Lm <- read.csv("scenario-la-Lm.csv") %>%
   gather(key = "model.run", value = "logLik") %>% 
   separate(model.run, into = c("model", "run"), sep = "\\.")
 
-# Vis ---------------------------------------------------------------------
-
-p1 <- ggplot(res_lm, aes(x = quantile, y = mean_ape, color = model)) +
-  geom_point(show.legend = TRUE) +
-  labs(y = parse(text = "alpha*'-'*quantile~of~APE"), x = parse(text = "alpha")) +
-  scale_color_discrete(name = element_blank(), labels = c("anchor", "plain")) +
-  theme(legend.position = c(0.2, 0.9)) +
-  labs(tag = "A")
-
 probs <- seq(0, 1, length.out = 1e3)
 
-p2 <- res_Lm %>% 
+# Vis ---------------------------------------------------------------------
+
+lmdat <- res_lm %>% 
+  group_by(model, run) %>% 
+  summarise(qfun = quantile(ape, probs = probs)) %>%
+  mutate(p = probs)
+
+slmdat <- lmdat %>% 
+  group_by(model, p) %>% 
+  summarise(median = median(qfun))
+
+p1 <- lmdat %>%  
+  ggplot(aes(x = p, y = qfun, group = paste(model, run), color = model)) +
+  geom_line(show.legend = FALSE, alpha = 0.05) +
+  geom_line(aes(x = p, y = median, group = model, color = model), 
+            data = slmdat, show.legend = TRUE, inherit.aes = FALSE) +
+  labs(x = parse(text = "alpha"), y = parse(text = "alpha*'-'*quantile~of~APE[i]")) +
+  theme(legend.position = c(0.2, 0.8), legend.key.size = ggplot2::unit(0.8, "line")) +
+  labs(tag = "a", color = element_blank())
+
+Lmdat <- res_Lm %>% 
   group_by(model, run) %>% 
   summarise(qfun = quantile(-logLik, probs = probs)) %>%
-  mutate(p = probs) %>% 
+  mutate(p = probs)
+
+sLmdat <- Lmdat %>% 
   group_by(model, p) %>% 
-  summarise(qfun = mean(qfun)) %>% 
-  ggplot(aes(x = p, y = qfun, group = model, color = model)) +
-  geom_line(show.legend = FALSE) +
-  labs(x = parse(text = "alpha"), 
-       y = parse(text = "alpha*'-'*quantile~of~-logLik[i]")) +
-  labs(tag = "B")
+  summarise(median = median(qfun))
 
-p1 + p2
+p3 <- Lmdat %>%  
+  ggplot(aes(x = p, y = qfun, group = paste(model, run), color = model)) +
+  geom_line(show.legend = FALSE, alpha = 0.05) +
+  geom_line(aes(x = p, y = median, group = model, color = model), 
+            data = sLmdat, show.legend = FALSE, inherit.aes = FALSE) +
+  labs(x = parse(text = "alpha"), y = parse(text = "alpha*'-'*quantile~of~NLL[i]")) +
+  labs(tag = "b") +
+  ylim(0, 200)
 
-ggsave("figures/vis-la.pdf", height = 3, width = 7)
+p1 + p3
+
+ggsave("vis-la.jpg", height = 3, width = 7, dpi = 1200)
